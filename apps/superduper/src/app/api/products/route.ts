b93b394bf9914589ae54f7c4298c21faf45edb2b
Галт Б <24LP0532@G401-CC0100.local>
2024-10-18 17:28:21 +0800

    post

--- apps/superduper/src/app/api/products/route.ts
+++ apps/superduper/src/app/api/products/route.ts
@@ -1,15 +1,22 @@
-export async function GET() {
-  return Response.json({ message: 'Heelo world!' });
+import { DB } from "../../lib/db";
+
+
+
+export async function POST(request: Request) {
+  try {
+    const collection = DB.collection("product");
+    const newProduct = await request.json();
+    console.log(newProduct)
+    const result = await collection.insertOne(newProduct) 
+    console.log(result)
+    return Response.json(result,{status:200})
+  }
+  catch (error){
+    
+    return Response.json({ message: 'Failed to create product!' }, {status:404});
+  }
 }
 
-export async function POST() {
-  return Response.json({ message: 'Heelo world!' });
-}
 
-export async function PUT() {
-  return Response.json({ message: 'Heelo world!' });
-}
 
-export async function DEL() {
-  return Response.json({ message: 'Heelo world!' });
-}
+
