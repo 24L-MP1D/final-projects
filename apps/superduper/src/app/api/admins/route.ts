0fb7e07eb2565b5f3e08cd601e54081f51413394
ugankhuyag <urantsetseg.gan@gmail.com>
2024-10-20 20:23:50 +0800

    minor update

--- apps/superduper/src/app/api/admins/route.ts
+++ apps/superduper/src/app/api/admins/route.ts
@@ -1,19 +1,31 @@
-import { DB } from '../../lib/db';
+import { DB } from "../../lib/db";
 
 export async function GET(request: Request) {
   try {
-    const collection = DB.collection('seller');
-    const result = await collection.find({ name: 'badral' }).toArray();
-    return Response.json(result);
+    const collection = DB.collection('productApproveRequest');
+    const approve = await collection.find({}).toArray();
+    return Response.json(approve)
   } catch (err) {
-    console.error(err);
+    console.error(err)
   }
+  return Response.json({ message: 'approved' });
 }
 
 export async function POST(request: Request) {
   try {
-    const collection = DB.collection('products');
-  } catch (err) {
-    console.error(err);
+    const collection = DB.collection("productApproveRequest");
+    const approveApproveProduct = await request.json();
+    console.log(approveApproveProduct)
+    const approve = await collection.insertOne(approveApproveProduct) 
+    console.log(approve)
+    return Response.json(approve,{status:200})
+  }
+  catch (error){
+    
+    return Response.json({ message: 'Failed to approve the product!' }, {status:404});
   }
 }
+
+
+
+
