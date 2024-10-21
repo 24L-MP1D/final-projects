c16bcb170e76ff8f5a52ae8297e15320f74dfb22
24LP5207 24LP5207 <24LP5207@G401-CC0213.local>
2024-10-21 17:00:22 +0800

    jj

--- apps/superduper/src/app/client/layout.tsx
+++ apps/superduper/src/app/client/layout.tsx
@@ -1,9 +1,12 @@
+import { Header } from '../components/header';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   );
-
 }
