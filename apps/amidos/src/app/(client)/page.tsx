98892d9048d67b7f7ce4e9a22bd1b16f60546bde
anarchuluubayarsaikhan <anarchulu@gmail.com>
2024-10-21 19:52:08 +0800

    amidos(client homepage): client homepage header footer hero section done, comment on process

--- apps/amidos/src/app/(client)/page.tsx
+++ apps/amidos/src/app/(client)/page.tsx
@@ -1,7 +1,17 @@
+import Aboutlower from '../components/aboutlower';
+import Aboutupper from '../components/aboutupper';
+import Comments from '../components/comments';
+import Hero from '../components/hero';
+import Specialdishes from '../components/specialdishes';
+
 export default function Home() {
   return (
     <div>
-      <div>Client</div>
+      <Hero />
+      <Specialdishes />
+      <Aboutupper />
+      <Aboutlower />
+      <Comments />
     </div>
   );
 }
---
+++ apps/amidos/src/app/components/specialdishes.tsx
@@ -0,0 +1,62 @@
+'use client';
+import Image from 'next/image';
+
+import { EmblaPluginType } from 'embla-carousel';
+import Autoplay from 'embla-carousel-autoplay';
+import { useEffect, useRef, useState } from 'react';
+import { Carousel, CarouselApi, CarouselContent, CarouselItem } from './ui/carousel';
+type PropType = {
+  plugins?: EmblaPluginType[];
+};
+
+export default function Specialdishes() {
+  const plugin = useRef(Autoplay({ delay: 2000, stopOnInteraction: true }));
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on('select', () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+
+  const specialdishes = [
+    { url: '/carbonara.jpg', price: 21000, name: 'Carbonara' },
+    { url: '/pasta.jpg', price: 15000, name: 'Pasta' },
+    { url: '/pizza.jpeg', price: 25000, name: 'Pizza' },
+  ];
+
+  return (
+    <Carousel
+      setApi={setApi}
+      plugins={[
+        Autoplay({
+          delay: 2000,
+          stopOnInteraction: false,
+          stopOnMouseEnter: true,
+        }),
+      ]}
+    >
+      <CarouselContent>
+        {specialdishes.map((special) => (
+          <CarouselItem>
+            <div className="w-[471px] h-[391px] aspect-video overflow-hidden relative shadow-2xl" key={special.name}>
+              <Image src={special.url} alt="Image of food" width={471} height={391} className="w-full h-full object-cover" />
+              <div className="text-white absolute bottom-1 left-4">
+                <p className="text-xl font-semibold">{special.name}</p>
+                <p>{special.price}₮</p>
+              </div>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+    </Carousel>
+  );
+}
