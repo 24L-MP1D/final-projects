ea6a4d9051f4bd1ecaeaa3db1db5502d13fbf8a6
24LP2640 24LP2640 <24LP2640@hishigee.local>
2024-10-21 19:50:00 +0800

    acers(home section): adds param changer (holiday)
    git push

--- apps/acers/src/app/(client)/page.tsx
+++ apps/acers/src/app/(client)/page.tsx
@@ -1,10 +1,8 @@
 'use client';
 
 import { Star, Bookmark } from 'lucide-react';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { useState } from 'react';
-
-
 
 export default function Index() {
   /*
@@ -14,15 +12,45 @@ export default function Index() {
    */
   return (
     <div className="">
-      <div className="flex flex-col gap-16 max-w-[1160px] w-full m-auto">
+      <div className="flex flex-col gap-16 max-w-[1160px] w-full m-auto text-[#222222]">
         <RecipeOfTheDay />
+        <OccasionMeals />
       </div>
     </div>
   );
 }
 
+const OccasionMeals = () => {
+  const searchParams = useSearchParams();
+  const holiday = searchParams.get("holiday");
+  const handleHolidayChange = (name:string) => {
+    const newSearchParams = new URLSearchParams(searchParams.toString());
+    newSearchParams.set("holiday", name);
+    
+    window.history.pushState({}, '', `?${newSearchParams.toString()}`);
+  };
+
+  return (
+    <div className="border-t-2 border-[#222222] max-w-[1160px] w-full m-auto flex flex-col">
+      <span className="text-[23px]">Make Something to Celebrate</span>
+      <span className="text-[17px]">Delicious recipes for Canadian Thanksgiving, Halloween, Diwali and more.</span>
+      <div className="mt-2 flex gap-1 flex-wrap">
+        {holidays.map((day) => (
+          <span
+            key={day.name}
+            onClick={() => handleHolidayChange(day.name)}
+            className={`py-3 px-4 rounded-[4px] whitespace-nowrap ${holiday === day.name ? 'bg-[#fddc79]' : ' hover:bg-[#fddc79]'}`}
+          >
+            {day.name}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
 const RecipeOfTheDay = () => {
-  const router = useRouter()
+  const router = useRouter();
   const [data, setData] = useState({
     img: 'https://img.freepik.com/free-photo/fresh-pasta-with-hearty-bolognese-parmesan-cheese-generated-by-ai_188544-9469.jpg?semt=ais_hybrid',
     title: 'Malaay Qumbe (Coconut Fish Curry)',
@@ -36,7 +64,7 @@ const RecipeOfTheDay = () => {
   return (
     <div className="flex items-center gap-10">
       <div className="relative bg-slate-500">
-        <img src={img} className={`w-[710px] object-center`} onClick={()=>router.push(`/product/${id}`)} />
+        <img src={img} className={`w-[710px] object-center`} onClick={() => router.push(`/product/${id}`)} />
         <SaveButton id={id} className="absolute right-6 bottom-6" />
       </div>
       <div className="flex flex-col text-[#222222]">
@@ -57,7 +85,7 @@ export const Stars = ({ rating, voteNum, id }: { rating: number; voteNum: number
         {Array(5)
           .fill(1)
           .map((_, index) => (
-            <Star fill={(Math.round(rating) >= index + 1 && '#222222') || '#CCCCCC'} strokeWidth={0} key={(`${id}_${index}`)} />
+            <Star fill={(Math.round(rating) >= index + 1 && '#222222') || '#CCCCCC'} strokeWidth={0} key={`${id}_${index}`} />
           ))}
       </div>
       <span className="text-[12px]">{voteNum}</span>
@@ -81,3 +109,16 @@ export const SaveButton = ({ id, className }: { id: string; className: string })
     </button>
   );
 };
+
+const holidays = [
+  { name: "New Year's Day" },
+  { name: "Valentine's Day" },
+  { name: 'Easter' },
+  { name: 'Independence Day' },
+  { name: 'Halloween' },
+  { name: 'Thanksgiving' },
+  { name: 'Christmas' },
+  { name: 'Diwali' },
+  { name: 'Hanukkah' },
+  { name: 'Lunar New Year' },
+];
