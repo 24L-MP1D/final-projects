e66ed01e0ae3d566f6497f45f3a8d4cbea1e6627
24LP5207 24LP5207 <24LP5207@G401-CC0213.local>
2024-10-22 17:02:26 +0800

    changed

--- apps/superduper/src/app/client/page.tsx
+++ apps/superduper/src/app/client/page.tsx
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-"use client"
-import Image from "next/image";
-import { useEffect, useState } from "react";
-interface Product {
-    name: string,
-    image_url: string,
-}
-
-export default function Home() {
-    const [products, setProducts] = useState<Product[]>([]);
-    useEffect(() => {
-
-        const fetchProducts = async () => {
-            const res = await fetch('/api/products');
-            const data = await res.json();
-            setProducts(data)
-        };
-        fetchProducts();
-    }, []);
-
-    return (
-        <div>
-            {products.map((product) => (
-                <div>{product.name}
-                    <Image src={product.image_url} alt="image" height={280} width={280} className="h-[280px] w-[280px]" />
-                </div>
-
-            ))}
-        </div>
-    )
-}
-=======
 'use client';
 interface Product {
   name: string;
@@ -39,4 +6,3 @@ interface Product {
 export default function Home() {
   return <div></div>;
 }
->>>>>>> 09537c3 (superduper: added header)
