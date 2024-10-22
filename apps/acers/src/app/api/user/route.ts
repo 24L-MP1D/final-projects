bc95a08af9a5ca62cd8e17d64cbab58d9e093c63
24LP2640 24LP2640 <24LP2640@hishigee.local>
2024-10-22 17:27:06 +0800

    acers(deploy): ready for build

--- apps/acers/src/app/api/user/route.ts
+++ apps/acers/src/app/api/user/route.ts
@@ -3,8 +3,8 @@ import { DB } from '../../lib/db';
 export async function POST(request: Request) {
   try {
     const res = await DB.collection('Users').insertOne({ name: 'Zolo', age: 20 });
-    return res;
+    return Response.json(res);
   } catch (e) {
-    return e;
+    return Response.json(e);
   }
 }
