677d5b3fe830f29edf682846e4c7a9299794f81c
24LP2121 24LP2121 <24LP2121@G401-CC0101.local>
2024-10-22 17:18:08 +0800

    superduper: home page barg bolsoon

--- apps/superduper/src/app/page.tsx
+++ apps/superduper/src/app/page.tsx
@@ -1,10 +1,14 @@
 "use client"
+import { ChevronRight } from 'lucide-react';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { Swiper as SwiperType } from "swiper";
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+import { Autoplay, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide, } from 'swiper/react';
 import { CardBody, CardContainer, CardItem } from './components/ui/card';
 
 interface Product {
@@ -25,101 +29,156 @@ export default function Index() {
     };
     fetchProducts();
   }, []);
-  return (
-    // <Swiper className='mt-[100px] h-[500px] w-[500px]'
-    //   modules={[Navigation, Pagination, Scrollbar, A11y, EffectFade]}
 
-    //   scrollbar={{ draggable: true }}
-    // >
-    //   {slides.slice(0, 5).map((slide) => (
-    //     <div>
-    //       <SwiperSlide><Image alt='' src={slide.image_url} width={100} height={100} /></SwiperSlide>
-    //     </div>
-    //   ))}
-    // </Swiper>
-    <div className='grid grid-cols-4'>
-      {
-        slides.slice(0, 8).map((slide) => (
-          <div>
-            < CardContainer className="inter-var" >
-              <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
-                <CardItem
-                  translateZ="50"
-                  className="text-xl font-bold text-neutral-600 dark:text-white"
-                >
-                  {slide.name}
-                </CardItem>
-                <CardItem
-                  as="p"
-                  translateZ="60"
-                  className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-                >
-                  {slide.description}
-                </CardItem>
-                <CardItem translateZ="100" className="w-full mt-4">
-                  <Image alt='ds' src={slide.image_url} width={500} height={500} />
-                </CardItem>
-                <CardItem
-                  translateZ="50"
-                  className="text-xl font-bold text-neutral-600 dark:text-white"
-                >
-                  {slide.category}
-                </CardItem>
-                <CardItem
-                  translateZ="50"
-                  className="text-xl font-bold text-neutral-600 dark:text-white"
-                >
-                  {slide.starting_price}
-                </CardItem>
-                <div className="flex justify-between items-center mt-20">
-                  <CardItem
-                    translateZ={20}
-                    href="https://twitter.com/mannupaaji"
-                    target="__blank"
-                    className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
-                  >
-                    Bid now →
-                  </CardItem>
-                </div>
-              </CardBody>
-            </CardContainer >
+  const images = [
+    "https://media.wired.com/photos/66180ed0c1ba76f5ce2ff268/master/w_2240,c_limit/Rolex.jpg",
+    "https://media.wired.com/photos/66180ed0c1ba76f5ce2ff268/master/w_2240,c_limit/Rolex.jpg",
+    "https://media.wired.com/photos/66180ed0c1ba76f5ce2ff268/master/w_2240,c_limit/Rolex.jpg",
+    "https://media.wired.com/photos/66180ed0c1ba76f5ce2ff268/master/w_2240,c_limit/Rolex.jpg",
+  ];
+
+
+  const swiperRef = useRef<SwiperType | null>(null);
+  const ref = useRef()
+  const [progress, setProgress] = useState(0);
+
+
+  const handleProgressClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    const newProgress = e.nativeEvent.offsetX / e.currentTarget.clientWidth;
+    setProgress(newProgress);
+    const totalSlides = images.length;
+    const targetSlideIndex = Math.floor(newProgress * totalSlides);
+    if (swiperRef.current) {
+      swiperRef.current.slideTo(targetSlideIndex);
+    }
+  };
+
+  const handleNextSlide = () => {
+    if (swiperRef.current) {
+      swiperRef.current.slideNext();
+    }
+  };
+
+  return (
+    <div className="max-w-[1220px] mx-auto w-full">
+      <div className='flex'>
+        <div className="gap-10 grid py-10">
+          <div className="flex gap-20">
+            <div className="grid gap-5">
+              <div className="text-[#565B60] text-sm">11 - 20 OCTOBER 2024</div>
+              <div className="text-[#0033FF] text-5xl font-semibold">Most Wanted</div>
+              <div className="text-[#565B60] text-sm">Get your hands on this year's most sought-after objects, from top luxury brands to
+                niche finds.
+              </div>
+              <div className="text-[#0033FF] text-sm">Explore now</div>
+            </div>
           </div>
-        ))
-      }
+          <div className="flex w-full mt-[100px] h-[40px]">
+            <div className="relative h-2 w-full bg-gray-300 mt-4">
+              <div
+                className="absolute top-0 left-0 h-full bg-blue-500"
+                style={{ width: `${progress * 100}%` }}
+              ></div>
+              <div
+                className="absolute top-0 left-0 h-full w-full cursor-pointer"
+                onClick={handleProgressClick}
+              ></div>
+            </div>
+            <button
+              className='items-center text-[#0033FF] ml-[5px]'
+              onClick={handleNextSlide}
+            >
+              <ChevronRight strokeWidth={1.75} />
+            </button>
+          </div>
+        </div>
+        <div className="w-[60%] py-10 pl-10">
+          <Swiper
+            direction={"vertical"}
+            slidesPerView={1}
+            spaceBetween={30}
+            loop={true}
+            autoplay={{
+              delay: 3000,
+              disableOnInteraction: false,
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Pagination, Autoplay]}
+            onSwiper={(swiper) => (swiperRef.current = swiper)} // Save swiper instance
+            onSlideChange={(swiper) => {
+              const newProgress = swiper.activeIndex / (images.length - 1);
+              setProgress(newProgress);
+            }}
+            style={{
+              height: "350px",
+            }}
+          >
+            {images.map((src, index) => (
+              <SwiperSlide key={index}>
+                <Image
+                  alt={`Slide ${index + 1}`}
+                  src={src}
+                  width={1200}
+                  height={600}
+                  className="w-full h-full object-cover"
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </div>
+      <div className='grid grid-cols-3 gap-x-10 max-w-[1220px] w-full'>
+        {
+          slides.slice(0, 6).map((slide) => (
+            <div className=''>
+              < CardContainer className="inter-var w-[450px] p-10" >
+                <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+                  <CardItem
+                    translateZ="50"
+                    className="text-xl font-bold text-neutral-600 dark:text-white"
+                  >
+                    {slide.name}
+                  </CardItem>
+                  <CardItem
+                    as="p"
+                    translateZ="60"
+                    className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                  >
+                    {slide.description}
+                  </CardItem>
+                  <CardItem translateZ="100" className="w-full mt-4">
+                    <Image alt='ds' src={slide.image_url} width={500} height={500} />
+                  </CardItem>
+                  <CardItem
+                    translateZ="50"
+                    className="text-xl font-bold text-neutral-600 dark:text-white"
+                  >
+                    {slide.category}
+                  </CardItem>
+                  <CardItem
+                    translateZ="50"
+                    className="text-xl font-bold text-neutral-600 dark:text-white"
+                  >
+                    {slide.starting_price}
+                  </CardItem>
+                  <div className="flex justify-between items-center mt-20">
+                    <CardItem
+                      translateZ={20}
+                      href="https://twitter.com/mannupaaji"
+                      target="__blank"
+                      className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                    >
+                      Bid now →
+                    </CardItem>
+                  </div>
+                </CardBody>
+              </CardContainer >
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
-};
-
-// export default function Index() {
-//   return (
-//     <div className="max-w-[1220px] mx-auto w-full">
-//       <div className="grid p-10 gap-10">
-//         <div className="flex gap-20">
-//           <div className="grid gap-5">
-//             <div className="text-[#565B60] text-xs">11 - 20 OCTOBER 2024</div>
-//             <div className="text-[#0033FF] text-5xl font-semibold">Most Wanted</div>
-//             <div className="text-[#565B60] text-xs">Get your hands on this year's most sought-after objects, from top luxury brands to
-//               niche finds.
-//             </div>
-//             <div className="text-[#0033FF] text-xs">Explore now</div>
-//             <div className="flex gap-1 mt-[70px]  items-center">
-//               <button className="h-[4px] w-[70px] bg-[#0033FF]"></button>
-//               <button className="h-[4px] w-[70px] bg-[#D2D9E1]"></button>
-//               <button className="h-[4px] w-[70px] bg-[#D2D9E1]"></button>
-//               <button className="h-[4px] w-[70px] bg-[#D2D9E1]"></button>
-//               <button className="h-[4px] w-[70px] bg-[#D2D9E1]"></button>
-//               <button className="h-[4px] w-[70px] bg-[#D2D9E1]"></button>
-//               <button><ChevronRight size={20} strokeWidth={2} className="text-[#0033FF]" /></button>
-//             </div>
-//           </div>
-//           <div>
-//             <Image alt="" src="/images/img.png" width={600} height={300} />
-//           </div>
-//         </div>
-//         <div>
-//           <Products />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+}
