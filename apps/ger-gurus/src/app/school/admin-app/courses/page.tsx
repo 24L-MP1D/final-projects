52d29477d841eb02f7852ccf48e61094aa092d35
JavzaaBatmunkh <javzaabatmunkh@gmail.com>
2024-10-22 18:32:34 +0800

    create

--- apps/ger-gurus/src/app/school/admin-app/courses/page.tsx
+++ apps/ger-gurus/src/app/school/admin-app/courses/page.tsx
@@ -1,8 +1,9 @@
 import { Button } from "@/src/app/components/ui/button";
+import Link from "next/link";
 
 export default function Page() {
     return (<div>
-        <Button>Add new course</Button>
+        <Link href="/school/admin-app/courses/create"><Button>Add new course</Button></Link>
 
     </div>)
 }
