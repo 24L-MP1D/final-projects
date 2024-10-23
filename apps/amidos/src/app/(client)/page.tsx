d16831bf10fb1e42a74bc1e3fc452d04651e3464
anarchuluubayarsaikhan <anarchulu@gmail.com>
2024-10-23 16:49:02 +0800

    amidos(client): delivery part adding

--- apps/amidos/src/app/(client)/page.tsx
+++ apps/amidos/src/app/(client)/page.tsx
@@ -1,7 +1,6 @@
 import Aboutlower from '../components/aboutlower';
 import Aboutupper from '../components/aboutupper';
 import Comments from '../components/comments';
-import { Locationgeo } from '../components/geolocation';
 import { Googlemap } from '../components/googlemap';
 import Hero from '../components/hero';
 import Specialdishes from '../components/specialdishes';
