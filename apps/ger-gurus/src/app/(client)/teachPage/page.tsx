3a7034e889d30087afe4a20bdd9306dc1e8f19e9
24LP4781 24LP4781 <24LP4781@administrators-iMac-2.local>
2024-10-23 16:58:09 +0800

    ger-gurus-school

--- apps/ger-gurus/src/app/(client)/teachPage/page.tsx
+++ apps/ger-gurus/src/app/(client)/teachPage/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import { SchoolPageHeader } from "../../components/schoolPageHeader";
+import { SchoolPageIntroduction } from "../../components/schoolPageIntroduction";
 
 export default function Page() {
   return (
     <div className=" ">
         <SchoolPageHeader />
-    
+        <SchoolPageIntroduction />
     </div>
   );
 }
