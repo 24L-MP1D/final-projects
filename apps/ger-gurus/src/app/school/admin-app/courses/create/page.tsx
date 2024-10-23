4062a7fd975933890bbe96c795bd03965511d3f1
JavzaaBatmunkh <javzaabatmunkh@gmail.com>
2024-10-23 11:35:58 +0800

    upload

--- apps/ger-gurus/src/app/school/admin-app/courses/create/page.tsx
+++ apps/ger-gurus/src/app/school/admin-app/courses/create/page.tsx
@@ -8,15 +8,19 @@ export default function Page() {
     const [title, setTitle] = useState<string>("");
     const [author, setAuthor] = useState<string>("");
     const [description, setDescription] = useState<string>("");
+    const [images, setImages] = useState<string[]>([]);
+    const [files, setFiles] = useState<FileList[]>([]);
 
 
     async function createCourse() {
+        const images = await handleUpload();
         await fetch(`/api/courses`, {
             method: "POST",
             body: JSON.stringify({
                 title,
                 author,
-                description
+                description,
+                thumbnail: images
             }),
             headers: { "Content-type": "application/json; charset=UTF-8" },
         });
@@ -50,7 +54,23 @@ export default function Page() {
             Array.from(fileList ?? []).forEach((file) => {
                 formData.append("image", file, file.name);
             });
-        });
+        })
+        console.log(formData);
+        try {
+        //   setLoading(true);
+          const response = await fetch("/api/upload", {
+            method: "POST",
+            body: formData,
+          });
+          const data = await response.json();
+          // console.log(data.secure_url)
+        //   setLoading(false);
+    
+          return data;
+        } catch (error) {
+          console.error("error uploading file:", error);
+        }
+    };
 
 
         return (<div className="flex flex-col gap-4 w-[50%]">
@@ -62,7 +82,7 @@ export default function Page() {
                 onChange={(e) => setDescription(e.target.value)} />
             <div className="grid w-full max-w-sm items-center gap-1.5">
                 <Label htmlFor="picture">Thumbnail Picture</Label>
-                <Input id="picture" type="file" />
+                <Input id="picture" type="file" onChange={handleFileChange}/>
             </div>
             <Button onClick={createCourse}>Continue</Button>
 
