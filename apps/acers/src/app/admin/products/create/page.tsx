b6a597b60030874392ab94aa0935571585825a4c
24LP2640 24LP2640 <24LP2640@hishigee.local>
2024-10-24 15:57:23 +0800

    acers(admin add prod): lalalala

--- apps/acers/src/app/admin/products/create/page.tsx
+++ apps/acers/src/app/admin/products/create/page.tsx
@@ -1,30 +1,38 @@
+'use client';
+
 import { SubmitHandler, useForm } from 'react-hook-form';
+import { Input } from '../../../(client)/components/ui/Input';
+import { ImageInput } from './imageInput';
+import { VideoInput } from './videoInput';
 
 const Page = () => {
   interface RecipeForm {
     title: string;
-    description: string; // Corrected the typo here
-    images: string[];
+    description: string;
+    imagesFile: File[];
     prepTime: string;
     servings: number;
-    video: string;
+    videoFile: File | null;
     ingredients: string[];
     instructions: string[];
     nutritionFacts: { name: string; value: string }[];
     category: string | null;
     difficulty: number;
-    availability: 'Gold' | 'Silver' | 'Bronze' | 'Free'; // Fixed the string syntax
+    availability: 'Gold' | 'Silver' | 'Bronze' | 'Free';
+    images: string[];
+    video: string;
   }
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<RecipeForm>();
 
   const onSubmit: SubmitHandler<RecipeForm> = (data) => {
     Submit(data);
   };
-
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm<RecipeForm>();
 
   function Submit(data: RecipeForm) {
     console.log(data);
@@ -33,12 +41,25 @@ const Page = () => {
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register('title')} placeholder="Title" />
-        <input {...register('description')} placeholder="Description" />
+        <div className="grid grid-cols-2 gap-4">
+          <ComponentParent>
+            <Input {...register('title')} placeholder="Title" />
+            <Input {...register('description')} placeholder="Description" />
+          </ComponentParent>
+          <ComponentParent>
+            <ImageInput register={(name: string, value: File[]) => setValue(name, value)} />
+            <VideoInput register={(name: string, value: File | null) => setValue(name, value)} />
+          </ComponentParent>
+        </div>
+
         <button type="submit">Submit</button>
       </form>
     </div>
   );
+};
+
+const ComponentParent = ({ children }: { children: React.ReactNode }) => {
+  return <div className="p-4 flex flex-col gap-4">{children}</div>;
 };
 
 export default Page;
