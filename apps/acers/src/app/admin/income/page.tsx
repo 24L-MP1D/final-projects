0758f9cea98565015dcca3fe3bba1fdea3f69282
Oyundari23 <dariaa0560@gmail.com>
2024-10-24 15:21:15 +0800

    dashboard in process

--- apps/acers/src/app/admin/income/page.tsx
+++ apps/acers/src/app/admin/income/page.tsx
@@ -7,29 +7,29 @@ export default function Home() {
   return (
     <div className="flex ">
       <DashboardAside />
-      <div className="flex flex-col gap-4">
-        <div className="h-[100px] w-[200px] rounded-xl bg-pink-200 p-3 ml-5 mt-4">Income amount </div>
-        <Table className="border-[1px] border-[#d1d5db] ml-5 rounded-xl">
-          <TableCaption>Орлогын журнал </TableCaption>
-          <TableHeader>
-            <TableRow className="justify-between">
-              <TableHead>Нэхэмжлэх</TableHead>
-              <TableHead>Төлбөрийн статус</TableHead>
-              <TableHead>Төлбөрийн хэлбэр</TableHead>
-              <TableHead className="">Төлбөрийн дүн</TableHead>
-              <TableHead className="">Төлбөрийн дүн</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            <TableRow className="justify-between">
-              <TableCell className="font-medium">INV001</TableCell>
-              <TableCell>Paid</TableCell>
-              <TableCell>Credit Card</TableCell>
-              <TableCell className="">$250.00</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </div>
+      <div className="text-center">INCOME</div>
+      {/* <div className="h-[100px] w-[200px] rounded-xl bg-pink-200 p-3 ml-5 mt-4">Income amount </div> */}
+      <Table className="border-[1px] border-[#d1d5db] w-[800px] ml-5 mt-5 rounded-xl bg-white ">
+        <TableCaption>Орлогын журнал </TableCaption>
+        <TableHeader className="">
+          <TableRow className=" flex justify-between">
+            <TableHead>Нэхэмжлэх</TableHead>
+            <TableHead>Төлбөрийн статус</TableHead>
+            <TableHead>Төлбөрийн хэлбэр</TableHead>
+            <TableHead>Төлбөрийн дүн</TableHead>
+            <TableHead>Төлбөрийн дүн</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow className="flex justify-between">
+            <TableCell className="font-medium">Invoice ID</TableCell>
+            <TableCell>PaymentStatus</TableCell>
+            <TableCell>PaymentMethod</TableCell>
+            <TableCell>Amount</TableCell>
+            <TableCell>Amount</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </div>
   );
 }
--- apps/acers/src/app/admin/user/page.tsx
+++ apps/acers/src/app/admin/user/page.tsx
@@ -1,12 +1,34 @@
 'use client';
 
 import { DashboardAside } from '../components /DashboardAside';
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '../components /ui/Table';
 
 export default function Home() {
   return (
     <div className="flex">
       <DashboardAside />
-      user
+      <div className="text-center text-bold text-center"> USER </div>
+      <Table className="border-[1px] border-[#d1d5db] ml-5 mt-5  w-[800px] rounded-xl bg-white  ">
+        <TableCaption>Хэрэглэгчийн бүртгэлийн жагсаалт </TableCaption>
+        <TableHeader>
+          <TableRow className="justify-between">
+            <TableHead>Username</TableHead>
+            <TableHead>ID</TableHead>
+            <TableHead>Email</TableHead>
+            <TableHead className="">phoneNumber</TableHead>
+            <TableHead className="">Role</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow className="justify-between">
+            <TableCell className="font-medium">.userName</TableCell>
+            <TableCell>._id</TableCell>
+            <TableCell>.email</TableCell>
+            <TableCell className="">.phoneNumber</TableCell>
+            <TableCell className="">.Role</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </div>
   );
 }
