8511ccb4e4657dadac90de57ebfed92dfd50b93c
JavzaaBatmunkh <javzaabatmunkh@gmail.com>
2024-10-24 11:42:37 +0800

    main code

--- apps/ger-gurus/src/app/school/admin-app/courses/page.tsx
+++ apps/ger-gurus/src/app/school/admin-app/courses/page.tsx
@@ -76,12 +76,4 @@ export default function Page() {
 
             </TableBody>
         </Table>
-
-import { Button } from "@/src/app/components/ui/button";
-
-export default function Page() {
-    return (<div>
-        <Button>Add new course</Button>
-
-    </div>)
-}
+        </div>)}
