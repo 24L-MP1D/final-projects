5a48758ef042066ee639e599df336de28f4ccc57
odbaatar tulgaa <metatulgaa@gmail.com>
2024-10-28 12:38:12 +0800

    button click

--- apps/superduper/src/app/page.tsx
+++ apps/superduper/src/app/page.tsx
@@ -1,134 +1,113 @@
-"use client"
-import { ChevronRight, Heart } from 'lucide-react';
+'use client';
+
+import { ChevronRight } from 'lucide-react';
 import Image from 'next/image';
 import { useEffect, useRef, useState } from 'react';
-import { Swiper as SwiperType } from "swiper";
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import { Swiper as SwiperType } from 'swiper';
 import { Autoplay, Pagination } from 'swiper/modules';
-import { Swiper, SwiperSlide, } from 'swiper/react';
-import { CardBody, CardContainer, CardItem } from './components/ui/card';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Product, ProductItem } from './components/productItem';
 
-interface Product {
-  name: string,
-  description: string,
-  category: string,
-  starting_price: number,
-  image_url: string,
-}
+const images = [
+  'https://media.wired.com/photos/66180ed0c1ba76f5ce2ff268/master/w_2240,c_limit/Rolex.jpg',
+  'https://media.wired.com/photos/66180ed0c1ba76f5ce2ff268/master/w_2240,c_limit/Rolex.jpg',
+  'https://media.wired.com/photos/66180ed0c1ba76f5ce2ff268/master/w_2240,c_limit/Rolex.jpg',
+  'https://media.wired.com/photos/66180ed0c1ba76f5ce2ff268/master/w_2240,c_limit/Rolex.jpg',
+];
 
 export default function Index() {
+  // Product data codes
+  const [products, setProducts] = useState<Product[]>([]);
 
-  const [slides, setSlides] = useState<Product[]>([])
   useEffect(() => {
     const fetchProducts = async () => {
       const res = await fetch('/api/products');
       const data = await res.json();
-      setSlides(data)
+      console.log(data);
+
+      setProducts(data);
     };
     fetchProducts();
   }, []);
 
-  const images = [
-    "https://media.wired.com/photos/66180ed0c1ba76f5ce2ff268/master/w_2240,c_limit/Rolex.jpg",
-    "https://media.wired.com/photos/66180ed0c1ba76f5ce2ff268/master/w_2240,c_limit/Rolex.jpg",
-    "https://media.wired.com/photos/66180ed0c1ba76f5ce2ff268/master/w_2240,c_limit/Rolex.jpg",
-    "https://media.wired.com/photos/66180ed0c1ba76f5ce2ff268/master/w_2240,c_limit/Rolex.jpg",
-  ];
-
-
-  const swiperRef = useRef<SwiperType | null>(null);
-  const ref = useRef()
+  // SWIPER codes
   const [progress, setProgress] = useState(0);
+  const swiperRef = useRef<SwiperType | null>(null);
 
-
-  const handleProgressClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    const newProgress = e.nativeEvent.offsetX / e.currentTarget.clientWidth;
-    setProgress(newProgress);
-    const totalSlides = images.length;
-    const targetSlideIndex = Math.floor(newProgress * totalSlides);
+  const handleProgressClick = (e: number) => {
     if (swiperRef.current) {
-      swiperRef.current.slideTo(targetSlideIndex);
+      swiperRef.current.slideTo(e);
+    }
+  };
+  const handleNextSlide = () => {
+    if (swiperRef.current) {
+      if (swiperRef.current.activeIndex === 3) {
+        swiperRef.current.slideTo(0);
+      } else {
+        swiperRef.current.slideNext();
+      }
     }
   };
 
-  const handleNextSlide = () => {
-    if (swiperRef.current) {
-      swiperRef.current.slideNext();
+  // Favourite codes
+  const [favourite, setFavourite] = useState<string[]>([]);
+
+  useEffect(() => {
+    const storage = localStorage.getItem('favourites');
+    if (storage) setFavourite(JSON.parse(storage));
+  }, []);
+
+  const handleFavourite = (productId: string) => {
+    let result: string[] = [...favourite];
+    if (result.find((id) => id === productId)) {
+      result = result.filter((id) => id !== productId);
+    } else {
+      result.push(productId);
     }
+
+    localStorage.setItem('favourites', JSON.stringify(result));
+    setFavourite(result);
   };
 
   return (
     <div className="max-w-[1220px] mx-auto w-full">
-      <div className='flex'>
+      <div className="flex">
         <div className="gap-10 grid py-10">
           <div className="flex gap-20">
             <div className="grid gap-5">
               <div className="text-[#565B60] text-sm">11 - 20 OCTOBER 2024</div>
               <div className="text-[#0033FF] text-5xl font-semibold">Most Wanted</div>
-              <div className="text-[#565B60] text-sm">Get your hands on this year's most sought-after objects, from top luxury brands to
-                niche finds.
-              </div>
+              <div className="text-[#565B60] text-sm">Get your hands on this years most sought-after objects, from top luxury brands to niche finds.</div>
               <div className="text-[#0033FF] text-sm">Explore now</div>
             </div>
           </div>
           <div className="flex w-full mt-[100px] h-[40px] gap-2">
             <div className="relative h-[7px] w-full bg-gray-300 mt-4 rounded-full">
-              <div
-                className="absolute top-0 left-0 h-full bg-blue-500 rounded-full"
-                style={{ width: `${progress * 100}%` }}
-              ></div>
-              <div
-                className="absolute top-0 left-0 h-full w-full cursor-pointer"
-                onClick={handleProgressClick}
-              ></div>
+              <div className="absolute top-0 left-0 h-full bg-blue-500 rounded-full" style={{ width: progress === 0 ? `${100}%` : 'auto' }}></div>
+              <div className="absolute top-0 left-0 h-full w-full cursor-pointer" onClick={() => handleProgressClick(0)}></div>
             </div>
             <div className="relative h-[7px] w-full bg-gray-300 mt-4 rounded-full">
-              <div
-                className="absolute top-0 left-0 h-full bg-blue-500 rounded-full"
-                style={{ width: `${progress * 100}%` }}
-              ></div>
-              <div
-                className="absolute top-0 left-0 h-full w-full cursor-pointer"
-                onClick={handleProgressClick}
-              ></div>
+              <div className="absolute top-0 left-0 h-full bg-blue-500 rounded-full" style={{ width: progress === 1 ? `${100}%` : 'auto' }}></div>
+              <div className="absolute top-0 left-0 h-full w-full cursor-pointer" onClick={() => handleProgressClick(1)}></div>
             </div>
             <div className="relative h-[7px] w-full bg-gray-300 mt-4 rounded-full">
-              <div
-                className="absolute top-0 left-0 h-full bg-blue-500 rounded-full"
-                style={{ width: `${progress * 100}%` }}
-              ></div>
-              <div
-                className="absolute top-0 left-0 h-full w-full cursor-pointer"
-                onClick={handleProgressClick}
-              ></div>
+              <div className="absolute top-0 left-0 h-full bg-blue-500 rounded-full" style={{ width: progress === 2 ? `${100}%` : 'auto' }}></div>
+              <div className="absolute top-0 left-0 h-full w-full cursor-pointer" onClick={() => handleProgressClick(2)}></div>
             </div>
             <div className="relative h-[7px] w-full bg-gray-300 mt-4 rounded-full">
-              <div
-                className="absolute top-0 left-0 h-full bg-blue-500 rounded-full"
-                style={{ width: `${progress * 100}%` }}
-              ></div>
-              <div
-                className="absolute top-0 left-0 h-full w-full cursor-pointer"
-                onClick={handleProgressClick}
-              ></div>
+              <div className="absolute top-0 left-0 h-full bg-blue-500 rounded-full" style={{ width: progress === 3 ? `${100}%` : 'auto' }}></div>
+              <div className="absolute top-0 left-0 h-full w-full cursor-pointer" onClick={() => handleProgressClick(3)}></div>
             </div>
-            <button
-              className='items-center text-[#0033FF] ml-[5px]'
-              onClick={handleNextSlide}
-            >
+            <button className="items-center text-[#0033FF] ml-[5px]" onClick={handleNextSlide}>
               <ChevronRight strokeWidth={1.75} />
             </button>
           </div>
         </div>
         <div className="w-[60%] py-10 pl-10">
           <Swiper
-            direction={"vertical"}
+            direction={'vertical'}
             slidesPerView={1}
             spaceBetween={30}
-            loop={true}
             autoplay={{
               delay: 3000,
               disableOnInteraction: false,
@@ -138,82 +117,24 @@ export default function Index() {
             }}
             modules={[Pagination, Autoplay]}
             onSwiper={(swiper) => (swiperRef.current = swiper)} // Save swiper instance
-            onSlideChange={(swiper) => {
-              const newProgress = swiper.activeIndex / (images.length - 1);
-              setProgress(newProgress);
-            }}
+            onSlideChange={(swiper) => setProgress(swiper.activeIndex)}
             style={{
-              height: "350px",
+              height: '360px',
             }}
           >
             {images.map((src, index) => (
               <SwiperSlide key={index}>
-                <Image
-                  alt={`Slide ${index + 1}`}
-                  src={src}
-                  width={1200}
-                  height={600}
-                  className="w-full h-full object-cover"
-                />
+                <Image alt={`Slide ${index + 1}`} src={src} width={1200} height={600} className="w-full h-full object-cover" />
               </SwiperSlide>
             ))}
           </Swiper>
         </div>
       </div>
-      <div className='grid grid-cols-3 gap-x-10 max-w-[1220px] w-full'>
-        {
-          slides.slice(0, 6).map((slide) => (
-            <div className=''>
-              < CardContainer className="inter-var w-[450px] p-10" >
-                <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
-                  <CardItem
-                    translateZ="50"
-                    className="text-xl font-bold text-neutral-600 dark:text-white flex text-nowrap gap-24"
-                  >
-                    {slide.name}
-                    <button className='rounded-full h-[40px] w-[40px] bg-white items-center flex justify-center text-[#0033FF]'><Heart size={22} strokeWidth={2} /></button>
-                  </CardItem>
-
-                  <CardItem
-                    as="p"
-                    translateZ="60"
-                    className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-                  >
-                    {slide.description}
-                  </CardItem>
-                  <CardItem translateZ="100" className="w-full mt-4">
-                    <Image alt='ds' src={slide.image_url} width={500} height={500} />
-                  </CardItem>
-                  <CardItem
-                    translateZ="50"
-                    className="text-xl font-bold text-neutral-600 dark:text-white"
-                  >
-                    {slide.category}
-                  </CardItem>
-                  <CardItem
-                    translateZ="50"
-                    className="text-xl font-bold text-neutral-600 dark:text-white"
-                  >
-                    {slide.starting_price}
-                  </CardItem>
-                  <div className="flex justify-between items-center mt-20">
-                    <CardItem
-                      translateZ={20}
-                      href="https://twitter.com/mannupaaji"
-                      target="__blank"
-                      className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
-                    >
-                      Bid now →
-                    </CardItem>
-                    <button>zahialah</button>
-                  </div>
-                </CardBody>
-              </CardContainer >
-            </div>
-          ))
-        }
+      <div className="grid grid-cols-3 gap-2 w-full">
+        {products.slice(0, 6).map((product) => (
+          <ProductItem product={product} key={product._id} onClickFavourite={() => handleFavourite(product._id)} isFavourite={!!favourite.find((id) => id === product._id)} />
+        ))}
       </div>
     </div>
   );
 }
-
