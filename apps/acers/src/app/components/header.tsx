39751dacd3bec9a583c790f321845d9da9f31df7
Ts3nguun <0805tsenguun@gmail.com>
2024-10-31 18:52:51 +0800

    navigation

--- apps/acers/src/app/components/header.tsx
+++ apps/acers/src/app/components/header.tsx
@@ -3,7 +3,7 @@
 import { Search, UserCircle } from 'lucide-react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import NavigationMenu from './navigationMenu';
+import HeaderNavigationMenu from './navigationMenu';
 import { Button } from './ui/button';
 import { Input } from './ui/input';
 import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
@@ -18,7 +18,10 @@ export function Component() {
           </Button>
         </SheetTrigger>
         <SheetContent className="bg-white  duration-700 inset-y-0 left-0 h-full w-4/4 ">
-          <div className="bg-white"></div>
+          <div className="bg-white">
+            <Search width={22} height={22} />
+            <Input className="border-none focus-visible:ring-0  bg-white " placeholder="What's you gonna eat?" />
+          </div>
         </SheetContent>
       </Sheet>
     </div>
@@ -31,15 +34,6 @@ function MenuIcon(props: any) {
       <line x1="4" x2="20" y1="12" y2="12" />
       <line x1="4" x2="20" y1="6" y2="6" />
       <line x1="4" x2="20" y1="18" y2="18" />
-    </svg>
-  );
-}
-
-function XIcon(props: any) {
-  return (
-    <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-      <path d="M18 6 6 18" />
-      <path d="m6 6 12 12" />
     </svg>
   );
 }
@@ -65,38 +59,36 @@ function SearchBar() {
 export default function Header() {
   const router = useRouter();
   return (
-    <div>
-      <div className=" mx-auto min-w-[310px] max-w-[1160px]">
-        <div className="flex justify-between items-center w-full lg:hidden">
-          <span className="flex gap-4 ">
-            <Component />
-            <SearchBar />
-          </span>
-          <Image width={50} height={45} alt="logo" src={'/Logo.png'} onClick={() => router.push(`/`)} />
-          <div className=" lg:hidden  items-center gap-2 flex ">
-            <button className="border bg-slate-400 text-white rounded-full leading-3 px-[19px]  py-[11px] hidden md:block" onClick={() => router.push(`/subscribe`)}>
-              Subscribe
-            </button>
-            <UserCircle width={24} height={24} />
-          </div>
-        </div>
-        <div className="hidden lg:flex justify-between w-full">
-          <Image width={70} height={65} alt="logo" src={'/Logo.png'} onClick={() => router.push(`/`)} />
-          <div className="flex h-full rounded-lg items-center border group focus-within:border-slate-700 pl-3 mt-4 p-1 gap-3 w-7/12">
-            <Search width={22} height={22} />
-            <Input className="border-none focus-visible:ring-0" placeholder="What's you gonna eat?" />
-          </div>
-          <div className="flex gap-1 items-center mt-1 ">
-            <button className="border border-gray-600 rounded-full font-bold leading-3 px-[19px] py-[11px] " onClick={() => router.push(`/login`)}>
-              Log In
-            </button>
-            <button className="border bg-slate-400 text-white rounded-full leading-3 px-[19px]  py-[11px]" onClick={() => router.push(`/subscribe`)}>
-              Subscribe
-            </button>
-          </div>
+    <div className=" mx-auto min-w-[310px] max-w-[1160px]">
+      <div className="flex justify-between items-center w-full lg:hidden">
+        <span className="flex gap-4 ">
+          <Component />
+          <SearchBar />
+        </span>
+        <Image width={50} height={45} alt="logo" src={'/Logo.png'} onClick={() => router.push(`/`)} />
+        <div className=" lg:hidden  items-center gap-2 flex ">
+          <button className="border bg-slate-400 text-white rounded-full leading-3 px-[19px]  py-[11px] hidden md:block" onClick={() => router.push(`/subscribe`)}>
+            Subscribe
+          </button>
+          <UserCircle width={24} height={24} />
         </div>
       </div>
-      <NavigationMenu />
+      <div className="hidden lg:flex justify-between w-full">
+        <Image width={70} height={65} alt="logo" src={'/Logo.png'} onClick={() => router.push(`/`)} />
+        <div className="flex h-full rounded-lg items-center border group focus-within:border-slate-700 pl-3 mt-4 p-1 gap-3 w-7/12">
+          <Search width={22} height={22} />
+          <Input className="border-none focus-visible:ring-0" placeholder="What's you gonna eat?" />
+        </div>
+        <div className="flex gap-1 items-center mt-1 ">
+          <button className="border border-gray-600 rounded-full font-bold leading-3 px-[19px] py-[11px] " onClick={() => router.push(`/login`)}>
+            Log In
+          </button>
+          <button className="border bg-slate-400 text-white rounded-full leading-3 px-[19px]  py-[11px]" onClick={() => router.push(`/subscribe`)}>
+            Subscribe
+          </button>
+        </div>
+      </div>
+      <HeaderNavigationMenu />
     </div>
   );
 }
