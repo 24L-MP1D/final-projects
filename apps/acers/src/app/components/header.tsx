00fec15ef700cd61f1cccfa5d84c7d31ed52e5b2
Ts3nguun <0805tsenguun@gmail.com>
2024-10-31 14:41:32 +0800

    navigation

--- apps/acers/src/app/components/header.tsx
+++ apps/acers/src/app/components/header.tsx
@@ -3,6 +3,7 @@
 import { Search, UserCircle } from 'lucide-react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import NavigationMenu from './navigationMenu';
 import { Button } from './ui/button';
 import { Input } from './ui/input';
 import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
@@ -94,14 +95,8 @@ export default function Header() {
             </button>
           </div>
         </div>
-        <div className="gap-5 h-12 mx-auto min-w-[310px] max-w-[1160px] hidden lg:flex">
-          <button className="font-bold">Want to Cook</button>
-          <button className="font-bold">Recipes</button>
-          <button className="font-bold">Ingredients</button>
-          <button className="font-bold">Ocassions</button>
-          <button className="font-bold">About</button>
-        </div>
       </div>
+      <NavigationMenu />
     </div>
   );
 }
---
+++ apps/acers/src/app/components/navigationMenu.tsx
@@ -0,0 +1,13 @@
+export default function NavigationMenu() {
+  return (
+    <div>
+      <div className="gap-5 h-12 mx-auto min-w-[310px] max-w-[1160px] hidden lg:flex">
+        <button className="font-bold">Want to Cook</button>
+        <button className="font-bold">Recipes</button>
+        <button className="font-bold">Ingredients</button>
+        <button className="font-bold">Ocassions</button>
+        <button className="font-bold">About</button>
+      </div>
+    </div>
+  );
+}
