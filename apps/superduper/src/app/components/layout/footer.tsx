559981c9e8c26213c43167f61e11d2441c27c849
Galtbatzana <164704929+Galtbatzana@users.noreply.github.com>
2024-10-31 14:05:15 +0800

    final

--- apps/superduper/src/app/components/layout/footer.tsx
+++ apps/superduper/src/app/components/layout/footer.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { BsInstagram } from 'react-icons/bs';
 import { FaFacebook, FaTwitter } from 'react-icons/fa';
+import { TbCopyright } from 'react-icons/tb';
 import { Button } from '../ui/button';
 
 export default function Footer() {
@@ -16,7 +17,7 @@ export default function Footer() {
       <div className="flex justify-evenly pb-10">
         <div className="flex flex-col items-start gap-2 mx-2 text-slate-400">
           <p className="font-semibold mb-4 text-black">About SuperDuper</p>
-          <Link href="/buy">About Super Duper</Link>
+          <Link href="/about">About Super Duper</Link>
           <Link href="/expert">Our experts</Link>
           <Link href="/carier">Carier</Link>
           <Link href="/press">Press</Link>
@@ -31,18 +32,18 @@ export default function Footer() {
         </div>
         <div className="flex flex-col items-start gap-2 mx-2 text-slate-400">
           <p className="font-semibold mb-4 text-black">Sell</p>
-          <Link href="/buy">How to sell</Link>
-          <Link href="/expert">Seller tips</Link>
-          <Link href="/carier">Submission guidelines</Link>
-          <Link href="/press">Seller terms</Link>
-          <Link href="/partner">Affiliates</Link>
+          <Link href="/sell">How to sell</Link>
+          <Link href="/sell">Seller tips</Link>
+          <Link href="/guide">Submission guidelines</Link>
+          <Link href="/sell">Seller terms</Link>
+          <Link href="/affiliate">Affiliates</Link>
           <Link href="/collector">Collectors Portal</Link>
         </div>
         <div className="flex flex-col items-start gap-2 mx-2 text-slate-400">
           <p className="font-semibold mb-4 text-black">My SuperDuper</p>
-          <Link href="/buy">Sign In</Link>
-          <Link href="/expert">Register</Link>
-          <Link href="/carier">Help center</Link>
+          <Link href="/client/sign-in">Sign In</Link>
+          <Link href="/client/sign-up">Register</Link>
+          <Link href="/help">Help center</Link>
         </div>
       </div>
       <div className="flex justify-between pt-10 mx-10 border-b-2 pb-10">
@@ -60,12 +61,15 @@ export default function Footer() {
         </div>
       </div>
       <div className="flex justify-around py-10 text-slate-400">
-        <Link href="/carier">Term of Use</Link>
-        <Link href="/carier">Data Protection & Privacy Notice</Link>
-        <Link href="/carier">Cookei Policy</Link>
-        <Link href="/carier">Law Enforcement Policy</Link>
-        <Link href="/carier">Other Policies</Link>
-        <div>C2024</div>
+        <Link href="/term">Term of Use</Link>
+        <Link href="/privacy">Data Protection & Privacy Notice</Link>
+        <Link href="/cookie">Cookei Policy</Link>
+        <Link href="/policy">Law Enforcement Policy</Link>
+        <Link href="/policy">Other Policies</Link>
+        <div className="flex items-center">
+          <TbCopyright className="text-black" />
+          <p className="text-black">2024</p>
+        </div>
       </div>
     </div>
   );
