3f7cc9db5a672b79eac7eb4c1c2530cc74467a2d
Delgermurun <delgermurun17@Delgermuruns-MacBook-Pro.local>
2024-11-04 17:27:57 +0800

    ger-gurus: login and signup

--- apps/ger-gurus/src/app/client/page.tsx
+++ apps/ger-gurus/src/app/client/page.tsx
@@ -3,6 +3,13 @@
 import { HomePageInfo } from '@/components/HomePageInfo';
 import { SchoolPageHeader } from '@/components/SchoolPageHeader';
 
+const urlParams = new URLSearchParams(window.location.search);
+const authtoken = urlParams.get('authtoken');
+if (authtoken) {
+  localStorage.setItem('authtoken', authtoken);
+  window.history.replaceState({}, document.title, '/');
+}
+
 export default function Page() {
   return (
     <div className=" ">
