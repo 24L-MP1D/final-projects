447a643d9f995a6e737a274267c13f79bf4d47a7
24LP5128 24LP5128 <24LP5128@Users-iMac.local>
2024-11-05 17:43:09 +0800

    superduper: translation on header and footer added

--- apps/superduper/src/app/client/addProducts/3/page.tsx
+++ apps/superduper/src/app/client/addProducts/3/page.tsx
@@ -124,8 +124,8 @@ export default function Page() {
               <div>
                 <Camera />
               </div>
-              <div>Click or drag and drop an image to upload</div>
-              <div className="mt-10">Front</div>
+              <div>Зураг дээр дарж эсвэл чирж буулгаж байршуулна уу</div>
+              <div className="mt-10">Нүүр</div>
 
               {formik.values.frontImage ? (
                 <div className="absolute w-full flex h-full justify-between z-50 bg-white">
@@ -151,8 +151,8 @@ export default function Page() {
               <div>
                 <Camera />
               </div>
-              <div>Click or drag and drop an image to upload</div>
-              <div className="mt-10">Back</div>
+              <div>Зураг дээр дарж эсвэл чирж буулгаж байршуулна уу</div>
+              <div className="mt-10">Буцах</div>
               {formik.values.backImage ? (
                 <div className="absolute w-full flex h-full justify-between z-50 bg-white">
                   <Image src={formik.values.backImage} alt="backImage" width={1000} height={1000} className="w-full h-full object-cover" />
@@ -178,8 +178,8 @@ export default function Page() {
               <div>
                 <Camera />
               </div>
-              <div>Click or drag and drop an image to upload</div>
-              <div className="mt-10">Details</div>
+              <div>Зураг дээр дарж эсвэл чирж буулгаж байршуулна уу</div>
+              <div className="mt-10">Дэлгэрэнгүй</div>
               {formik.values.detailImage ? (
                 <div className="absolute w-full flex h-full justify-between z-50 bg-white">
                   <Image src={formik.values.detailImage} alt="detailImage" width={1000} height={1000} className="w-full h-full object-cover" />
@@ -205,8 +205,8 @@ export default function Page() {
               <div>
                 <Camera />
               </div>
-              <div>Click or drag and drop an image to upload</div>
-              <div className="mt-10">Signature</div>
+              <div>Зураг дээр дарж эсвэл чирж буулгаж байршуулна уу</div>
+              <div className="mt-10">Гарын үсэг</div>
               {formik.values.signatureImage ? (
                 <div className="absolute w-full flex h-full justify-between z-50 bg-white">
                   <Image src={formik.values.signatureImage} alt="signatureImage" width={1000} height={1000} className="w-full h-full object-cover" />
@@ -232,8 +232,8 @@ export default function Page() {
               <div>
                 <Camera />
               </div>
-              <div>Click or drag and drop an image to upload</div>
-              <div className="mt-10">Damage</div>
+              <div>Зураг дээр дарж эсвэл чирж буулгаж байршуулна уу</div>
+              <div className="mt-10">Гэмтэл</div>
               {formik.values.damageImage ? (
                 <div className="absolute w-full flex h-full justify-between z-50 bg-white">
                   <Image src={formik.values.damageImage} alt="damageImage" width={1000} height={1000} className="w-full h-full object-cover" />
@@ -260,8 +260,8 @@ export default function Page() {
               <div>
                 <Camera />
               </div>
-              <div>Click or drag and drop an image to upload</div>
-              <div className="mt-10">Additional</div>
+              <div>Зураг дээр дарж эсвэл чирж буулгаж байршуулна уу</div>
+              <div className="mt-10">Нэмэлт</div>
 
               {formik.values.additionalImage ? (
                 <div className="absolute w-full flex h-full justify-between z-50 bg-white">
@@ -287,20 +287,20 @@ export default function Page() {
           </div>
           <div>
             <div className=" pb-12 border-b-2 border-black">
-              <div className="text-2xl">Tips:</div>
+              <div className="text-2xl">Зөвлөмж:</div>
               <ol className="flex flex-col gap-4 list-decimal px-5">
-                <li>One photo of entire item as close as possible</li>
-                <li>One detail shot, 25% of item unless item very smal</li>
-                <li>Take in bright day light. Turn on flash</li>
-                <li>Take at slight angle off center to avoid reflection and flash back</li>
-                <li>Check clarity on computer before uploading. Reshoot if not in focus</li>
-                <li>Remove from glass when possible</li>
-                <li>Show scale and context; details and texture</li>
-                <li>Use your own photos: we cannot accept a photograph of a photo of the work</li>
+                <li>Барааг бүтнээр нь оруулсан аль болох ойроос авсан нэг зураг</li>
+                <li>Бараа маш жижиг биш бол барааны 25% харуулсан нэг дэлгэрэнгүй зураг</li>
+                <li>Өдрийн хурц гэрэлд авах. Гэрлээ асаасан байх.</li>
+                <li>Тусгал болон гэрлийн ойлтыг оруулахгүйн тулд төвөөс нь бага зэрэг хазайлттай авах</li>
+                <li>Байршуулахаасаа өмнө компьютер дээрх тодорхой байдлыг шалгана уу. Хэрэв фокус нь таараагүй бол дахин зургыг авах</li>
+                <li>Боломжтой бол шилнээс нь гаргах</li>
+                <li>Хэмжээ ба контекстийг харуулах; дэлгэрэнгүй мэдээлэл ба бүтэц</li>
+                <li>Өөрийн зургийг ашиглана уу: бид ажлын зургийн зургийг хүлээн авах боломжгүй</li>
               </ol>
             </div>
             <div className="mt-4">
-              <div>Examples:</div>
+              <div>Жишээ нь:</div>
               <div className="mt-4 grid grid-cols-2 gap-x-2 gap-y-4">
                 <Image className="border-2" src={'/images/handbag.jpg'} alt="a" width={200} height={200} />
                 <Image className="border-2" src={'/images/handbag.jpg'} alt="a" width={200} height={200} />
@@ -310,11 +310,11 @@ export default function Page() {
             </div>
           </div>
           <div className="flex gap-2 w-full justify-center fixed bottom-0 bg-[#ffffff] py-2 left-[50%] translate-x-[-50%]">
-            <div>Click “continue” to save your progress for this step</div>
+            <div>"Үргэлжлүүлэх" дээр дарж энэ алхамын явцаа хадгална уу</div>
             <Link className="bg-slate-300 text-center py-2 px-4 rounded-lg" href={'/client/addProducts/2'}>
-              BACK
+            БУЦАХ
             </Link>
-            <Button type="submit">CONTINUE</Button>
+            <Button type="submit">ҮРГЭЛЖЛҮҮЛЭХ</Button>
           </div>
         </div>
       </div>
