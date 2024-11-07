ed52215db6033920b3d06d617bc3f33437a457b9
satsuralala <satsuraltumurbat@gmail.com>
2024-11-07 16:09:56 +0800

    superduper: cat's img

--- apps/superduper/src/app/api/categories/route.ts
+++ apps/superduper/src/app/api/categories/route.ts
@@ -1,14 +1,13 @@
 import { DB } from '@/lib/db';
 
-
 export async function POST(request: Request) {
   try {
     const collection = await DB.collection('categories');
-    const category=await request.json();
-    console.log(category)
-    const result=await collection.insertOne({category});
-    return new Response(null,{status:201})
+    const category = await request.json();
+
+    const result = await collection.insertOne(category);
+    return new Response(null, { status: 201 });
   } catch (err) {
-    return new Response(null,{status:404})
+    return new Response(null, { status: 404 });
   }
 }
