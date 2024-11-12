1ed499f07e0312847f2d2f3be54cabfaf6c83dfa
Zoljargal2004 <zolookorzoloo@gmail.com>
2024-11-12 15:33:48 +0800

    acers(protection): adds wrapper fixes create recipe

--- apps/acers/src/app/api/recipe/[id]/route.ts
+++ apps/acers/src/app/api/recipe/[id]/route.ts
@@ -21,44 +21,6 @@ async function verifyToken(request: Request) {
   }
 }
 
-export async function POST(request: Request) {
-  try {
-    const userId = await verifyToken(request);
-
-    const body = await request.json();
-    const date = new Date();
-    const { title, description, prepTime, servings, ingredients, instructions, nutritionFacts, category, difficulty, availability, images, video, tags, comment } = body;
-
-    const res = await DB.collection('recipes').insertOne({
-      title,
-      description,
-      prepTime,
-      servings,
-      ingredients,
-      instructions,
-      nutritionFacts,
-      category,
-      difficulty,
-      availability,
-      images,
-      video,
-      tags,
-      visits: 0,
-      createdAt: date.toDateString(),
-      updatedAt: date.toDateString(),
-      comment,
-    });
-
-    return new Response(JSON.stringify({ res: 'Succeed' }), { status: 200, headers: { 'Content-Type': 'application/json' } });
-  } catch (e) {
-    console.error(e);
-    return new Response(JSON.stringify({}), {
-      status: 500,
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
-}
-
 export async function GET(request: Request, { params }: { params: { id: string } | any }) {
   console.log('Fetching recipe with ID:', params?.id);
 
