7965e89e130d3ebd85e14ea59821e1fa617a8507
JavzaaBatmunkh <javzaabatmunkh@gmail.com>
2024-11-13 16:30:14 +0800

    school route

--- apps/ger-gurus/src/app/dashboard/admin-app/courses/[courseId]/chapters/[chapterId]/_components/chapter-description-form.tsx
+++ apps/ger-gurus/src/app/dashboard/admin-app/courses/[courseId]/chapters/[chapterId]/_components/chapter-description-form.tsx
@@ -96,20 +96,20 @@ export const ChapterDescriptionForm: React.FC<ChapterDescriptionFormProps> = ({ 
               </button>
             </div>
           </form>
-          <iframe
+          {/* <iframe
             width="560"
             height="315"
             src="https://www.youtube.com/embed/bqhwlU8qVsk?"
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowfullscreen
-          ></iframe>
+          ></iframe> */}
 
-          <iframe
+          {/* <iframe
             src="https://player.vimeo.com/video/821101511?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
             allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
             title="THANK YOU FOR NOT ANSWERING"
-          ></iframe>
+          ></iframe> */}
         </Form>
       )}
     </div>
